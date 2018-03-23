434502751e6ac52c88f0a8daa8e668998bb6ebe3
asd <asd>
2018-03-23 10:20:52 -0300

    clase 2

--- ejercicio/main.c
+++ ejercicio/main.c
@@ -13,16 +13,18 @@
 
 int main()
 {
-    int num, maxPar, cantPares, cantImpares, sumaPar, sumaImpar, cantEntre, max, min;
+    int num, maxPar, cantPares, cantImpares, sumaPos, sumaNeg, cantEntre, max, min, cantPos, cantNeg;
 
-    printf("Ingrese los numero (cero para salir)\n");
+    printf("Ingrese un numero \n");
     scanf("%d", &num);
 
     cantPares = 0;
     cantImpares = 0;
-    sumaImpar = 0;
-    sumaPar = 0;
     cantEntre = 0;
+    cantPos = 0;
+    cantNeg = 0;
+    sumaPos = 0;
+    sumaNeg = 0;
 
     while (num != 0)
     {
@@ -37,20 +39,28 @@ int main()
 
         if (num%2 == 0){
             cantPares++;
-            sumaPar += num;
             if (cantPares == 0) maxPar = num;
             else if(num > maxPar) maxPar = num;
         }
 
         else{
             cantImpares++;
-            sumaImpar += num;
         }
+
+        if (num < 0){
+            cantNeg++;
+            sumaNeg += num;
+        }
+        else{
+            cantPos++;
+            sumaPos += num;
+        }
+
 
         scanf("%d", &num);
     }
 
-    printf("%d --- %d --- %d --- %d --- %d --- %d --- %d --- %d", cantPares, cantImpares, sumaPar / cantPares, sumaImpar / cantImpares, cantEntre, max, min, maxPar);
+    printf("%d --- %d --- %.2f --- %.2f --- %d --- %d --- %d --- %d", cantPares, cantImpares, sumaPos / cantPos, sumaNeg / cantNeg, cantEntre, max, min, maxPar);
 
     return 0;
 }
